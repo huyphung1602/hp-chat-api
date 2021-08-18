c0111a0f1018b8e2657bd45100a707288479da01
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-19 00:24:15 +0700

    Update message response

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,14 @@
 module Api::V1
   class MessagesController < ApplicationController
     def index
-      @messages = Message.all
+      @messages = Message.all.includes(:user).map do |message|
+        {
+          id: message.id,
+          username: message.user.name,
+          body: message.body,
+        }
+      end
+
       json_response(@messages)
     end
   end
