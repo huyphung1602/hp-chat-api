8ab74f4dda47f25df9690d444c212f9e5c22f43f
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-19 18:27:47 +0700

    Restrict the resouce only allow api v1. Update sessions#create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,14 @@
 module Api::V1
   class SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create]
+
     def create
       @user = User.find_by(email: params[:email])
       if @user && @user.authenticate(params[:password])
-         sessions[:user_id] = @user.id
+         session[:user_id] = @user.id
       end
 
-      json_response({ user: @user.name, email: @user.email })
+      json_response({ id: @user.id, user: @user.name })
    end
   end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:8080'
 
-    resource '*',
+    resource '/api/v1/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
