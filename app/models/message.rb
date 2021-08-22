9e4b664f631b04f2c214f018bdf3ea563be1f0dc
huyphung1602 <huy.phung.sw@gmail.com>
2021-08-22 18:29:11 +0700

    Add a stupid broadcasting

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  after_create_commit :broadcast_to_room_channel
+
+  def broadcast_to_room_channel
+    ActionCable.server.broadcast(
+      "room_#{self.room.id}_channel",
+      {
+        id: self.id,
+        content: self.content,
+        owner: self.user.name
+      }
+    )
+  end
 end
